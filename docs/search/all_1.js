8605c758b22b4bf95417586423e59cd7df1e360e
Keshav Bhatt <keshavnrj@gmail.com>
2023-03-25 21:47:16 +0530

    chore(doc): update documentation

--- docs/search/all_1.js
+++ docs/search/all_1.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['cacheexists_0',['cacheExists',['../classCachier.html#a03535bc0fbf4a121711309b3552e751e',1,'Cachier::cacheExists(const std::size_t &amp;hash)'],['../classCachier.html#afc3fc622eb1a706e17a666091f30cffe',1,'Cachier::cacheExists(const std::string &amp;filename)']]],
+  ['cacheexists_0',['cacheExists',['../classCachier.html#a427732270473941ef43ffac642f3f835',1,'Cachier::cacheExists(const std::size_t &amp;key)'],['../classCachier.html#afc3fc622eb1a706e17a666091f30cffe',1,'Cachier::cacheExists(const std::string &amp;filename)']]],
   ['cachier_1',['Cachier',['../classCachier.html',1,'Cachier'],['../index.html',1,'(Global Namespace)']]],
   ['computehash_2',['computeHash',['../classCachier.html#aee2626d54a40751de3959b40614ece39',1,'Cachier']]]
 ];
--- docs/search/functions_1.js
+++ docs/search/functions_1.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['cacheexists_0',['cacheExists',['../classCachier.html#a03535bc0fbf4a121711309b3552e751e',1,'Cachier::cacheExists(const std::size_t &amp;hash)'],['../classCachier.html#afc3fc622eb1a706e17a666091f30cffe',1,'Cachier::cacheExists(const std::string &amp;filename)']]],
+  ['cacheexists_0',['cacheExists',['../classCachier.html#a427732270473941ef43ffac642f3f835',1,'Cachier::cacheExists(const std::size_t &amp;key)'],['../classCachier.html#afc3fc622eb1a706e17a666091f30cffe',1,'Cachier::cacheExists(const std::string &amp;filename)']]],
   ['computehash_1',['computeHash',['../classCachier.html#aee2626d54a40751de3959b40614ece39',1,'Cachier']]]
 ];
