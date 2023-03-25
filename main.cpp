1b40118349ed02a1f18cd2504cd01f43b00ea116
Keshav Bhatt <keshavnrj@gmail.com>
2023-03-25 21:40:08 +0530

    feat: add getContent test

--- main.cpp
+++ main.cpp
@@ -7,23 +7,27 @@ int main() {
   std::string target_file = "/tmp/target_file";
 
   Cachier cache("/tmp/my_chache_store",
-                  FileCacheOption::ENSURE_CACHE_STORE_PATH);
+                FileCacheOption::ENSURE_CACHE_STORE_PATH);
+
   if (cache.isInitialized()) {
 
     // add a file to the cache
-    std::size_t key = cache.addCacheFile(target_file, "king",
-                                         FileCacheOption::DONT_OVERWRITE_CACHE);
+    Cachier::HashResult cache_result = cache.addCacheFile(
+        target_file, "king", FileCacheOption::OVERWRITE_CACHE);
+
+    auto key = cache_result.key;
+    auto error = cache_result.error;
 
     if (key != 0) {
       std::cout << "File " << target_file << " cached as "
                 << std::to_string(key) << std::endl;
-    } else if (key == 00) {
-      auto cacheKey = cache.computeHash(target_file);
+    } else if (key == 1) {
+      auto hash_result = cache.computeHash(target_file);
 
       std::cout << "File " + target_file + " already exists in cache as "
-                << cacheKey << std::endl;
+                << hash_result.key << std::endl;
     } else {
-      std::cout << "Failed to create cache." << std::endl;
+      std::cout << error << std::endl;
     }
 
     // test Cachier::fileExists
@@ -42,15 +46,41 @@ int main() {
     std::transform(check_cache.begin(), check_cache.end(), check_cache.begin(),
                    [](unsigned char c) { return std::tolower(c); });
 
+    bool file_in_cache = false;
+
     if (check_cache == "y" || check_cache.empty()) {
-      if (cache.cacheExists(target_file)) {
+      file_in_cache = cache.cacheExists(target_file);
+      if (file_in_cache) {
         std::cout << "File " + target_file + " is in cache!" << std::endl;
       } else {
         std::cout << "File " + target_file +
-                         " is not in cache(probably modified!)"
+                         " is not in cache(or may have modified!)"
                   << std::endl;
       }
     }
+
+    // test Cachier::getContent
+    std::string get_content;
+
+    std::cout << std::endl
+              << "Test: getContent"
+              << "Get content stored for cache entry " << cache_result.key
+              << "? [y]" << std::endl;
+
+    std::getline(std::cin, get_content);
+
+    // convert input to lowercase
+    std::transform(check_cache.begin(), check_cache.end(), check_cache.begin(),
+                   [](unsigned char c) { return std::tolower(c); });
+
+    // get content
+    if (get_content == "y" || get_content.empty()) {
+      if (file_in_cache) {
+        std::cout << cache.getContent(std::to_string(cache_result.key))
+                  << std::endl;
+      }
+    }
+
     return 0;
   } else {
     return 1;
